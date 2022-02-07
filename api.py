fdba6d30a2446de0629c4414993a3370b1039f0c
T2brozz <42860763+T2brozz@users.noreply.github.com>
2022-02-07 12:53:49 +0100

    some api stuff

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
-
-
+import redis
+import datetime
+r=redis.Redis()
 app = FastAPI()
 
 
@@ -11,13 +12,24 @@ async def read_item():
 
 @app.get("/brightness/get")
 async def get_brightness():
-    return {"brightness": shared_values["brightness"]}
+    return {"brightness": await r.get("brightness")}
 
 
 @app.post("/brightness/set")
 async def set_brightness(new_brightness: int):
     if new_brightness > 100 or new_brightness < 0:
         return HTTPException(status_code=406, detail="Value out of Range")
-
-    shared_values["brightness"] = new_brightness
+    print(new_brightness)
+    await r.set("brightness",new_brightness)
     return {"action": "successful"}
+
+@app.post("/text/set")
+async def set_text(line1="",line2="",line3=""):
+    await r.set("line1",line1)
+    await r.set("line2",line2)
+    await r.set("line3",line3)
+    return {"action": "successful"}
+
+@app.post("/autodisplay/")
+async  def set_display_auto_onoff(on:datetime.time):
+        pass
